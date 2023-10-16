0aec0396eef28e8bd28f0fbddf9e4a4ee83afb92
Seth Legaspi <145301206+xcllyn@users.noreply.github.com>
2023-10-16 08:09:14 +0800

    Add files via upload
    yung arithmetic chuchu whatever notes nalang to

---
+++ Legaspi.java
@@ -0,0 +1,31 @@
+package legaspi;
+
+public class Legaspi {
+    public static void main(String[] args) {
+        
+        
+        int gp = 10000;
+        int gay = 500;
+        
+        float maasim = gp + gay;
+        System.out.println("addition: " + maasim);
+        
+        float subt = gp - gay;
+        System.out.println("subtraction: " + subt);
+        
+        float multi = gp * gay;
+        System.out.println("multiplication: " + multi);
+        
+        float div = gp / gay;
+        System.out.println("divition: " + div); 
+        
+        int total = gp + gay;
+        float  tite = gp % total;
+        System.out.println("percent: " +tite+"%");
+        
+        System.out.println (--gp);
+        System.out.println (++gay);
+    }
+}
+
+        
